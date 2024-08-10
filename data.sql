2b8dd41992a019c6d1dcd3738e8e0d68b0bd34bf
Joe Ton <joeton07@gmail.com>
2024-08-10 08:26:47 -0700

    Add new database and tables

---
+++ data.sql
@@ -0,0 +1,38 @@
+# Create DATABASE
+CREATE DATABASE IF NOT EXISTS Sales;
+USE Sales;
+
+# Create TABLE
+CREATE TABLE IF NOT EXISTS Product (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(255),
+    SupplierID INT,
+    CategoryID INT,
+    QuantityPerUnit VARCHAR(255),
+    UnitPrice DECIMAL(10,2),
+    UnitsInStock INT,
+    UnitsOnOrder INT,
+    ReorderLevel INT,
+    Discontinued INT
+);
+
+CREATE TABLE IF NOT EXISTS Category (
+    CategoryID INT PRIMARY KEY,
+    CategoryName VARCHAR(255),
+    Description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Supplier (
+    SupplierID INT PRIMARY KEY,
+    CompanyName VARCHAR(255),
+    ContactName VARCHAR(255),
+    ContactTitle VARCHAR(255),
+    Address VARCHAR(255),
+    City VARCHAR(255),
+    Region VARCHAR(255),
+    PostalCode VARCHAR(255),
+    Country VARCHAR(255),
+    Phone VARCHAR(255),
+    Fax VARCHAR(255),
+    HomePage TEXT
+);
