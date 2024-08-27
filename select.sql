d90c3259b1ceafe0b64c02603b59688145b7a514
Joe Ton <joeton07@gmail.com>
2024-08-27 07:14:52 -0700

    New logic

--- select.sql
+++ select.sql
@@ -17,3 +17,127 @@ FROM
     employees
 WHERE
     first_name = 'Kellie' and gender = 'F';
+
+    SELECT 
+    first_name
+FROM
+    employees
+WHERE
+    gender = 'F'
+        AND (first_name = 'Kellie'
+        OR first_name = 'Aruna');
+
+SELECT 
+    first_name
+FROM
+    employees
+WHERE
+    first_name IN ('Denis' , 'Elvis');
+
+SELECT 
+    first_name
+FROM
+    employees
+WHERE
+    first_name NOT IN ('john' , 'mark', 'jacob');
+    
+SELECT 
+    first_name
+FROM
+    employees
+WHERE
+    first_name LIKE ('mark%');
+    
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    hire_date LIKE ('%2000%');
+    
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    emp_no LIKE ('1000_');
+ 
+select * from employees where first_name like ('%JACK%');
+
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name NOT LIKE ('%jack%');
+    
+SELECT 
+    *
+FROM
+    salaries
+WHERE
+    emp_no NOT BETWEEN '10004' AND '10012';
+
+SELECT 
+    *
+FROM
+    salaries
+WHERE
+    salary BETWEEN 66000 AND 70000;
+    
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    gender = 'F' AND hire_date >= '2000-1-1';
+
+SELECT DISTINCT
+    hire_date
+FROM
+    employees;
+    
+SELECT 
+    COUNT(salary)
+FROM
+    salaries
+WHERE
+    salary >= 100000;
+    
+SELECT 
+    COUNT(*)
+FROM
+    dept_manager;
+    
+SELECT 
+    *
+FROM
+    employees
+ORDER BY hire_date DESC;
+
+SELECT 
+    salary, COUNT(emp_no) AS emps_with_same_salary
+FROM
+    salaries
+WHERE
+    salary > 80000
+GROUP BY salary
+ORDER BY salary;
+
+SELECT 
+    emp_no, AVG(salary)
+FROM
+    salaries
+GROUP BY emp_no
+HAVING AVG(salary) > 120000
+ORDER BY emp_no;
+
+SELECT 
+    emp_no
+FROM
+    dept_emp
+WHERE
+    from_date > '2000-01-01'
+GROUP BY emp_no
+HAVING COUNT(from_date) > 1
+ORDER BY emp_no;
