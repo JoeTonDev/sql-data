8a275d8355682c92711d873bee8a45a1e4cafde1
Joe Ton <joeton07@gmail.com>
2024-09-02 09:18:09 -0700

    New logic

--- subquerries.sql
+++ subquerries.sql
@@ -58,3 +58,16 @@ FROM
     GROUP BY e.emp_no
     ORDER BY e.emp_no
     LIMIT 20) AS B;
+
+
+SELECT
+    *
+FROM
+    dept_manager
+WHERE
+    emp_no IN (SELECT
+            emp_no
+FROM
+    employees
+WHERE
+   hire_date BETWEEN '1990-01-01' AND '1995-01-01');
