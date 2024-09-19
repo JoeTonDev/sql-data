68a27898b60192a4a54be33ffae10bd4f033a187
Joe Ton <joeton07@gmail.com>
2024-09-19 10:09:28 -0700

    New table

--- data.sql
+++ data.sql
@@ -136,3 +136,8 @@ create table if not exists CustomerCustomerDemo (
     CustomerID INT,
     CustomerTypeID INT
 );
+
+create table if not exists CustomerDemographics (
+    CustomerTypeID INT PRIMARY KEY,
+    CustomerDesc TEXT
+);
