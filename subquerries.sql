f0af98bad2ec7b832eb0fc9e0e70012db44e0b8d
Joe Ton <joeton07@gmail.com>
2024-10-09 07:43:47 -0700

    New subquery

--- subquerries.sql
+++ subquerries.sql
@@ -85,3 +85,7 @@ where emp_no not in (select emp_no from dept_manager);
 select *
 from manager_ID
 where emp_no in (select emp_no from employees where hire_date between '1990-01-01' and '1995-01-01');
+
+select *
+from employees
+where emp_no not in (select emp_no from dept_manager where to_date > curdate());
