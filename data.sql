ade1e8a17d575d28719c3ad58a256ec442a2021b
Joe Ton <joeton07@gmail.com>
2024-11-18 16:47:47 -0800

    New table

--- data.sql
+++ data.sql
@@ -317,3 +317,9 @@ create table if not exists Shippers (
     CompanyName VARCHAR(255),
     Phone VARCHAR(255)
 );
+
+create table if not exists Region (
+    RegionID INT PRIMARY KEY,
+    RegionDescription VARCHAR(255)
+);
+
