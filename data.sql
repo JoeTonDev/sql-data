e85d5f8e02761624c86a1940dd0bf88873441cd6
Joe Ton <joeton07@gmail.com>
2024-11-22 09:51:57 -0800

    Add new table

--- data.sql
+++ data.sql
@@ -333,4 +333,10 @@ create table if not exists EmployeeTerritories (
     TerritoryID INT
 );
 
+create table if not exists CustomerCustomerDemo (
+    CustomerID INT,
+    CustomerTypeID INT
+);
+
+
 
