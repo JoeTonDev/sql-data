28f35b0a74b0f859f174cee4f78611dee4a004ea
Joe Ton <joeton07@gmail.com>
2024-12-02 07:23:22 -0800

    New table

--- data.sql
+++ data.sql
@@ -371,5 +371,16 @@ create table if not exists Categories (
     Description TEXT
 );
 
+create table if not exists Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerID INT,
+    EmployeeID INT,
+    OrderDate DATE,
+    RequiredDate DATE,
+    ShippedDate DATE,
+    ShipVia INT,
+    Freight DECIMAL(10,2),   
+);
+
 
 
