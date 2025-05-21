4f56aa1e7abc030d90a777b8e783f565b391eeb5
Joe Ton <joeton07@gmail.com>
2025-05-21 07:06:38 -0700

    New table

--- data.sql
+++ data.sql
@@ -470,3 +470,9 @@ create table if not exists OrderDetails (
     Discount DECIMAL(10,2)
 );
 
+create table if not exists Shippers (
+    ShipperID INT PRIMARY KEY,
+    CompanyName VARCHAR(255),
+    Phone VARCHAR(255)
+);
+
