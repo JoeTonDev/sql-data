e71a9845e41dd1e12b2ec0686335205f7fb3598e
Joe Ton <joeton07@gmail.com>
2025-05-23 07:48:12 -0700

    Add table

--- data.sql
+++ data.sql
@@ -481,4 +481,9 @@ create table if not exists Region (
     RegionDescription VARCHAR(255)
 );
 
+create table if not exists Territories (   
+    TerritoryID INT PRIMARY KEY,
+    TerritoryDescription VARCHAR(255),
+);
+
 
