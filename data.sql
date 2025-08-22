9db5a5b5b4ea2a9cb23daa7b66560e60661899f2
Joe Ton <joeton07@gmail.com>
2025-08-22 08:23:38 -0700

    Add new table

--- data.sql
+++ data.sql
@@ -644,4 +644,12 @@ create table if not exists Orders (
     Freight DECIMAL(10,2),   
 );
 
+create table if not exists OrderDetails (
+    OrderDetailID INT PRIMARY KEY,
+    OrderID INT,
+    ProductID INT,
+    UnitPrice DECIMAL(10,2),
+    Quantity INT,
+    Discount DECIMAL(10,2)
+);
 
