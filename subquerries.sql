d758cb434a7e117bedccdbf6c3ac0bfdb74ff929
Joe Ton <joeton07@gmail.com>
2025-10-03 09:33:21 -0700

    New subquery

--- subquerries.sql
+++ subquerries.sql
@@ -324,3 +324,4 @@ where emp_no in (select emp_no from employees where hire_date between '1990-01-0
 
 select * from employees
 where emp_no in (select emp_no from dept_manager where emp_no = 110022 or emp_no = 110039);
+
